2f5572d33d9691ec14f79aecc143548809c9e9ea
Wasymir <72921498+Wasymir@users.noreply.github.com>
2023-04-04 23:41:26 +0200

    Changed way of storing configuration in class.

--- src/main/java/me/kp56/timetables/configuration/Config.java
+++ src/main/java/me/kp56/timetables/configuration/Config.java
@@ -6,86 +6,75 @@ import java.util.Map;
 public class Config {
     private static Config config = new Config();
 
-    private Map<String, Object> contents = new HashMap<>();
+    private  Map<String, Boolean> onOffSettings = new HashMap<>();
+    private Map<String, Integer> valueSettings = new HashMap<>();
 
     private Config() {
         //just for now
-        contents.put("genetic.generation_size", 500);
-        contents.put("genetic.environments", -1);
-        contents.put("genetic.environment_timeout", 120);
-        contents.put("genetic.fix_tries", 5);
+        valueSettings.put("genetic.generation_size", 500);
+        valueSettings.put("genetic.environments", -1);
+        valueSettings.put("genetic.environment_timeout", 120);
+        valueSettings.put("genetic.fix_tries", 5);
 
-        contents.put("maximum_daily_subjects", 2);
-        contents.put("maximum_daily_lessons", 11);
+        valueSettings.put("maximum_daily_subjects", 2);
+        valueSettings.put("maximum_daily_lessons", 11);
 
-        contents.put("keep_track_of", 5);
+        valueSettings.put("keep_track_of", 5);
 
-        contents.put("history.connected", true);
-        contents.put("math_aa.connected", false);
-        contents.put("economics.connected", false);
-        contents.put("polish_a.connected", true);
-        contents.put("english_a.connected", false);
-        contents.put("german_b.connected", false);
-        contents.put("spanish_b.connected", false);
-        contents.put("biology.connected", false);
-        contents.put("chemistry.connected", true);
-        contents.put("physics.connected", false);
-        contents.put("computer_science.connected", true);
-        contents.put("french_b.connected", false);
-        contents.put("geography.connected", false);
+        onOffSettings.put("history.connected", true);
+        onOffSettings.put("math_aa.connected", false);
+        onOffSettings.put("economics.connected", false);
+        onOffSettings.put("polish_a.connected", true);
+        onOffSettings.put("english_a.connected", false);
+        onOffSettings.put("german_b.connected", false);
+        onOffSettings.put("spanish_b.connected", false);
+        onOffSettings.put("biology.connected", false);
+        onOffSettings.put("chemistry.connected", true);
+        onOffSettings.put("physics.connected", false);
+        onOffSettings.put("computer_science.connected", true);
+        onOffSettings.put("french_b.connected", false);
+        onOffSettings.put("geography.connected", false);
 
-        contents.put("history_hl.disabled", false);
-        contents.put("history_sl.disabled", false);
-        contents.put("math_aa_hl.disabled", false);
-        contents.put("math_aa_sl.disabled", false);
-        contents.put("math_ai_sl.disabled", false);
-        contents.put("economics_hl.disabled", false);
-        contents.put("economics_sl.disabled", false);
-        contents.put("polish_a_hl.disabled", false);
-        contents.put("polish_a_sl.disabled", false);
-        contents.put("english_b_hl.disabled", false);
-        contents.put("english_a_hl.disabled", false);
-        contents.put("english_a_sl.disabled", false);
-        contents.put("german_b_hl.disabled", false);
-        contents.put("german_b_sl.disabled", true);
-        contents.put("spanish_b_hl.disabled", false);
-        contents.put("spanish_b_sl.disabled", true);
-        contents.put("biology_hl.disabled", false);
-        contents.put("biology_sl.disabled", false);
-        contents.put("chemistry_hl.disabled", false);
-        contents.put("chemistry_sl.disabled", false);
-        contents.put("physics_hl.disabled", false);
-        contents.put("physics_sl.disabled", true);
-        contents.put("computer_science_hl.disabled", false);
-        contents.put("computer_science_sl.disabled", false);
-        contents.put("french_b_sl.disabled", false);
-        contents.put("french_b_hl.disabled", true);
-        contents.put("geography_sl.disabled", true);
-        contents.put("geography_hl.disabled", false);
-        contents.put("dsd.disabled", false);
-        contents.put("tok.disabled", false);
-        contents.put("z_wych.disabled", false);
-        contents.put("polish_no_examination.disabled", false);
+        onOffSettings.put("history_hl.disabled", false);
+        onOffSettings.put("history_sl.disabled", false);
+        onOffSettings.put("math_aa_hl.disabled", false);
+        onOffSettings.put("math_aa_sl.disabled", false);
+        onOffSettings.put("math_ai_sl.disabled", false);
+        onOffSettings.put("economics_hl.disabled", false);
+        onOffSettings.put("economics_sl.disabled", false);
+        onOffSettings.put("polish_a_hl.disabled", false);
+        onOffSettings.put("polish_a_sl.disabled", false);
+        onOffSettings.put("english_b_hl.disabled", false);
+        onOffSettings.put("english_a_hl.disabled", false);
+        onOffSettings.put("english_a_sl.disabled", false);
+        onOffSettings.put("german_b_hl.disabled", false);
+        onOffSettings.put("german_b_sl.disabled", true);
+        onOffSettings.put("spanish_b_hl.disabled", false);
+        onOffSettings.put("spanish_b_sl.disabled", true);
+        onOffSettings.put("biology_hl.disabled", false);
+        onOffSettings.put("biology_sl.disabled", false);
+        onOffSettings.put("chemistry_hl.disabled", false);
+        onOffSettings.put("chemistry_sl.disabled", false);
+        onOffSettings.put("physics_hl.disabled", false);
+        onOffSettings.put("physics_sl.disabled", true);
+        onOffSettings.put("computer_science_hl.disabled", false);
+        onOffSettings.put("computer_science_sl.disabled", false);
+        onOffSettings.put("french_b_sl.disabled", false);
+        onOffSettings.put("french_b_hl.disabled", true);
+        onOffSettings.put("geography_sl.disabled", true);
+        onOffSettings.put("geography_hl.disabled", false);
+        onOffSettings.put("dsd.disabled", false);
+        onOffSettings.put("tok.disabled", false);
+        onOffSettings.put("z_wych.disabled", false);
+        onOffSettings.put("polish_no_examination.disabled", false);
     }
 
-    public Object getToken(String id) {
-        return contents.get(id);
-    }
-
-    public String getString(String id) {
-        return (String) getToken(id);
-    }
-
-    public boolean getBoolean(String id) {
-        return (Boolean) getToken(id);
+    public Boolean getBoolean(String id) {
+        return onOffSettings.get(id);
     }
 
     public int getInteger(String id) {
-        return (Integer) getToken(id);
-    }
-
-    public double getDouble(String id) {
-        return (Double) getToken(id);
+        return valueSettings.get(id);
     }
 
     public static Config getInstance() {
